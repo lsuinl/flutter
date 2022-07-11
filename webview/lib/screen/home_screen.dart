ad5b575a4e8c1c147a5f934c702ebcf026acd2b4
lsuinl <su96in43@naver.com>
2022-07-11 17:33:13 +0900

    2022.07.11

---
+++ webview/lib/screen/home_screen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class HomeScreen extends StatelessWidget {
+  WebViewController? controller;
+  final homeUrl= 'https://velog.io/@su96in43';
+  HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.indigo,
+        title: Text('수인이'),
+        centerTitle: true,
+        actions: [
+          IconButton(
+              onPressed: (){
+                if(controller == null){
+                  return;
+                }
+                controller!.loadUrl(homeUrl);
+              },
+              icon: Icon(
+                Icons.home,
+              ),
+          ),
+        ],
+      ),
+      body: WebView(
+        onWebViewCreated: (WebViewController controller){
+          this.controller = controller;
+        },
+        initialUrl: homeUrl,
+        javascriptMode: JavascriptMode.unrestricted,
+      )
+    );
+  }
+}
